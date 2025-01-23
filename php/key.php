aab4fcb1d6277217d84ce463d9ee237df32999c2
john <84650390+Reposity23@users.noreply.github.com>
2025-01-24 01:23:34 +0800

    Add files via upload

---
+++ php/key.php
@@ -0,0 +1,4 @@
+<?php
+// Set up API key
+//https://platform.openai.com/account/api-keys
+$API_KEY = "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";
